053c6f8beee4c7cda05b63627b417165356e6a3f
EphraimHaber <61934858+EphraimHaber@users.noreply.github.com>
2025-06-15 20:07:26 +0300

    fixed build and go to definition

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@repo/backend-api-client': resolve(__dirname, '../../packages/backend-api-client/src'),
+    },
+  },
+  optimizeDeps: {
+    include: ['@repo/backend-api-client'],
+  },
+  build: {
+    sourcemap: true,
+  },
+});
